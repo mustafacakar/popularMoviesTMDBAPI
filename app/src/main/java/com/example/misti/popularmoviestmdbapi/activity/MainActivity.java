779b05e590fa9d4d0c54e3abd9b86fa372bdee0f
master <mistik_cakar@hotmail.com>
2018-08-09 11:10:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/misti/popularmoviestmdbapi/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.misti.popularmoviestmdbapi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.misti.popularmoviestmdbapi.Adapter.MoviesAdapter;
+import com.example.misti.popularmoviestmdbapi.Model.Movie;
+import com.example.misti.popularmoviestmdbapi.Model.MoviesResponse;
+import com.example.misti.popularmoviestmdbapi.R;
+import com.example.misti.popularmoviestmdbapi.Rest.ApiClient;
+import com.example.misti.popularmoviestmdbapi.Rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // TODO - insert your themoviedb.org API KEY here
+    private final static String API_KEY = "2628d1c86529e4ff3b39af76f592d8cb";
+
+    //Retrofit will download and parse the API data on a background thread, and then return the
+    // results back to the UI thread via the onResponse or onFailure method.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
